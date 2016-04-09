8b33866d420f444554a7452e0ef652bcbe991811
Jay Whang <jaywhang83@gmail.com>
2016-04-08 18:59:11 -0700

    Add styling for favorite list

--- app/components/display-favorite.js
+++ app/components/display-favorite.js
@@ -2,4 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   favoriteList: Ember.inject.service(),
+
+  actions: {
+    pressed: function() {
+      this.$('.collapsible').collapsible({
+        accordion: true
+      });
+    }
+  }
 });
