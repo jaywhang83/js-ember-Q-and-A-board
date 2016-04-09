fd9d27e02a98393c81163095f9b77d3b40d08467
Jay Whang <jaywhang83@gmail.com>
2016-04-08 18:40:38 -0700

    Add favorite list service

--- app/components/answer-detail.js
+++ app/components/answer-detail.js
@@ -1,11 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoriteList: Ember.inject.service(),
+
   actions: {
     delete(answer) {
       if(confirm('Are you sure you want to delete this answer?')) {
         this.sendAction('destroyAnswer', answer);
       }
+    },
+
+    addToFavorite(favorite) {
+      this.get('favoriteList').add(favorite);
     }
-  }  
+  }
 });
